65a295c7bb47eca42528df7fface3d704879c8f7
Connie Chen <conne.chen1101@users.noreply.github.com>
2021-04-03 13:31:24 -0400

    Intializing

--- src/Cafe/OrderingCoffeeController.java
+++ src/Cafe/OrderingCoffeeController.java
@@ -1,11 +1,26 @@
 package Cafe;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
 
 public class OrderingCoffeeController {
 
+   @FXML
+   CheckBox caramelCheckbox, syrupCheckbox, milkCheckbox, creamCheckbox, whippedCheckbox;
 
-   void getAddins(ActionEvent action){
+   @FXML
+   TextField priceTextfield;
 
+   @FXML
+   ComboBox sizeCombobox;
+
+
+   void addCoffee(ActionEvent event){
+      
    }
 }
